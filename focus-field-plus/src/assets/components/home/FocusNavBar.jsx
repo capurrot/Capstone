3d20353f4b3d8d4d2ba4f4d932863b7bcf1b29f9
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2025-04-29 10:49:51 +0000

    image refactor

--- focus-field-plus/src/assets/components/home/FocusNavBar.jsx
+++ focus-field-plus/src/assets/components/home/FocusNavBar.jsx
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Logo from "../../../assets/images/logo.png";
+import Logo from "../../../../public/images/logo.png";
 import { useEffect, useState } from "react";
 import i18n from "i18next";
 import { useTranslation } from "react-i18next";
