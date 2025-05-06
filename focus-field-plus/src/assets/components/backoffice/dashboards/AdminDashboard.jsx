88c950d10ebab86658cf6e389bd4956306f4e294
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2025-05-06 09:40:32 +0000

    user nome e cognome

--- focus-field-plus/src/assets/components/backoffice/dashboards/AdminDashboard.jsx
+++ focus-field-plus/src/assets/components/backoffice/dashboards/AdminDashboard.jsx
@@ -2,11 +2,13 @@ import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
 const AdminDashboard = ({ user }) => {
   return (
-    <Container fluid className="py-4 bg-white">
+    <Container fluid className="py-4 bg-white text-black">
       <Container>
-        <h1 className="mb-4">Admin Dashboard </h1>
+        <h1>Admin Dashboard </h1>
         <Row className="g-4">
-          Ciao {user.nome} {user.cognome}
+          <p>
+            Ciao, {user.nome} {user.cognome}
+          </p>
         </Row>
       </Container>
     </Container>
